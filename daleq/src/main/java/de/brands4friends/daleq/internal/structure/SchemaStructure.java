396fc8f964749126517e6ea77e9d122db16feffc
Lars Girndt <l.girndt@googlemail.com>
2012-05-05 12:09:03 +0200

    implemented hashCode & equals + UT

--- daleq/src/main/java/de/brands4friends/daleq/internal/structure/SchemaStructure.java
+++ daleq/src/main/java/de/brands4friends/daleq/internal/structure/SchemaStructure.java
@@ -1,4 +1,35 @@
 package de.brands4friends.daleq.internal.structure;
 
+import java.util.List;
+
+import com.google.common.base.Objects;
+
 public class SchemaStructure {
+
+    private final List<TableStructure> tables;
+
+    public SchemaStructure(final List<TableStructure> tables) {
+        this.tables = tables;
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if (obj instanceof SchemaStructure) {
+            final SchemaStructure that = (SchemaStructure) obj;
+
+            return Objects.equal(tables, that.tables);
+        }
+
+        return false;
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hashCode(tables);
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this).add("tables",tables).toString();
+    }
 }
