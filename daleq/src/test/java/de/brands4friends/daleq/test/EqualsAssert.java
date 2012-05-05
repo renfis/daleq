1c3ec9e84e362198a22fa1a1bc2a286d91443646
Lars Girndt <l.girndt@googlemail.com>
2012-05-05 12:01:03 +0200

    add EqualsAssert

---
+++ daleq/src/test/java/de/brands4friends/daleq/test/EqualsAssert.java
@@ -0,0 +1,13 @@
+package de.brands4friends.daleq.test;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+
+public class EqualsAssert {
+
+    public static <T> void assertProperEqualsAndHashcode(final Class<T> clazz) {
+        final EqualsVerifier<T> verifier = EqualsVerifier.forClass(clazz).suppress(Warning.STRICT_INHERITANCE);
+        verifier.verify();
+    }
+
+}
