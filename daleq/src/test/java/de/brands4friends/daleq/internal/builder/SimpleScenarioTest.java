cdfec39135928cd421c48ae06c71b6d900826270
Lars Girndt <l.girndt@googlemail.com>
2012-05-05 16:03:22 +0200

    provide examples

--- daleq/src/test/java/de/brands4friends/daleq/internal/builder/SimpleScenarioTest.java
+++ daleq/src/test/java/de/brands4friends/daleq/internal/builder/SimpleScenarioTest.java
@@ -3,18 +3,19 @@ package de.brands4friends.daleq.internal.builder;
 import static de.brands4friends.daleq.Daleq.aRow;
 import static de.brands4friends.daleq.Daleq.aTable;
 import static de.brands4friends.daleq.PropertyDef.pd;
+import static de.brands4friends.daleq.internal.builder.SimpleScenarioTest.MyTable.NAME;
+import static de.brands4friends.daleq.internal.builder.SimpleScenarioTest.MyTable.VALUE;
 
 import org.dbunit.dataset.datatype.DataType;
 import org.junit.Test;
 
 import de.brands4friends.daleq.PropertyDef;
 import de.brands4friends.daleq.Table;
-import de.brands4friends.daleq.TableDef;
 
 public class SimpleScenarioTest {
 
-    @TableDef("MY_TABLE")
-    public static class MyTable {
+    @de.brands4friends.daleq.TableDef("foo")
+    public final static class MyTable {
         public static final PropertyDef ID       = pd(DataType.INTEGER);
         public static final PropertyDef NAME     = pd(DataType.VARCHAR);
         public static final PropertyDef VALUE    = pd(DataType.BIT);
@@ -22,7 +23,16 @@ public class SimpleScenarioTest {
     }
 
     @Test
-    public void aSimpleScenario() {
+    public void justTwoRows() {
         Table table = aTable(MyTable.class).with(aRow(1),aRow(2));
+    }
+
+    @Test
+    public void rowsWithProperties() {
+        Table table = aTable(MyTable.class)
+                .with(
+                        aRow(42).p(NAME, "foo").p(VALUE, "1"),
+                        aRow(23).p(NAME, "bar")
+                );
     }
 }
