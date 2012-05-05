3844a558649bfd862bf82c1b07999ce8a4fa965f
Lars Girndt <l.girndt@googlemail.com>
2012-05-05 17:00:22 +0200

    added context

---
+++ daleq/src/main/java/de/brands4friends/daleq/internal/builder/Context.java
@@ -0,0 +1,7 @@
+package de.brands4friends.daleq.internal.builder;
+
+import de.brands4friends.daleq.legacy.conversion.TypeConversion;
+
+public interface Context {
+    public TypeConversion getTypeConversion();
+}
