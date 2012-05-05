072dcbd235d8125029d1de3f1d6afe1c0eaadca9
Lars Girndt <l.girndt@googlemail.com>
2012-05-05 12:39:46 +0200

    basic def scanner.

---
+++ daleq/src/main/java/de/brands4friends/daleq/internal/PropertyDefScanner.java
@@ -0,0 +1,45 @@
+package de.brands4friends.daleq.internal;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Collection;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+import de.brands4friends.daleq.PropertyDef;
+
+public class PropertyDefScanner {
+
+    public <T> Collection<PropertyDef> scan(Class<T> fromClass)  {
+
+        try {
+            final List<PropertyDef> result = Lists.newArrayList();
+            for(Field field : fromClass.getDeclaredFields()){
+                if(isConstant(field) && isPropertyDef(field)){
+                    result.add((PropertyDef) field.get(null));
+                }
+            }
+
+            if(result.isEmpty()){
+                throw new IllegalArgumentException(
+                        "No PropertyType Definitions in class '" + fromClass.getSimpleName() + "'");
+            }
+
+            return result;
+        } catch (IllegalAccessException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private boolean isPropertyDef(final Field field) {
+        return field.getType().isAssignableFrom(PropertyDef.class);
+    }
+
+    private boolean isConstant(final Field field) {
+        final int modifiers = field.getModifiers();
+        return Modifier.isStatic(modifiers)
+           && Modifier.isFinal(modifiers)
+           && Modifier.isPublic(modifiers);
+    }
+}
---
+++ daleq/src/test/java/de/brands4friends/daleq/internal/PropertyDefScannerTest.java
@@ -0,0 +1,63 @@
+package de.brands4friends.daleq.internal;
+
+import static de.brands4friends.daleq.PropertyDef.pd;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Collection;
+
+import org.dbunit.dataset.datatype.DataType;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+import de.brands4friends.daleq.PropertyDef;
+
+public class PropertyDefScannerTest {
+
+    private PropertyDefScanner scanner;
+
+    @Before
+    public void setUp() throws Exception {
+        scanner = new PropertyDefScanner();
+    }
+
+    static class WithoutPropertyDefs {
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void scanningAClassWithoutAPropertyDef_should_fail(){
+        scanner.scan(WithoutPropertyDefs.class);
+    }
+
+    static class WithNonStatic {
+        public final PropertyDef ID   = pd("id", DataType.INTEGER);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void scanningWithNonStatic_should_fail(){
+        scanner.scan(WithNonStatic.class);
+    }
+
+    static class WithNonFinal {
+        public static PropertyDef ID   = pd("id", DataType.INTEGER);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void scanningWithNonFinal_should_fail(){
+        scanner.scan(WithNonFinal.class);
+    }
+
+    static class WithPropertyDefs {
+        public static final PropertyDef ID   = pd("id", DataType.INTEGER);
+        public static final PropertyDef NAME = pd("name", DataType.VARCHAR);
+    }
+
+    @Test
+    public void scanningAClassWithPropertyDefs_should_extractThosePropertyDefs(){
+        final Collection<PropertyDef> expected = Lists.newArrayList(WithPropertyDefs.ID,WithPropertyDefs.NAME);
+        assertThat(scanner.scan(WithPropertyDefs.class), is(expected));
+    }
+}
