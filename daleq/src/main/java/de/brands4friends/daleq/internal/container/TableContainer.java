c3b9a4906d22c02ab070b25d8356c9ed481b243d
Lars Girndt <l.girndt@googlemail.com>
2012-05-06 13:56:14 +0200

    added fields.

--- daleq/src/main/java/de/brands4friends/daleq/internal/container/TableContainer.java
+++ daleq/src/main/java/de/brands4friends/daleq/internal/container/TableContainer.java
@@ -1,5 +1,35 @@
 package de.brands4friends.daleq.internal.container;
 
-public class TableContainer {
+import java.util.List;
 
+import com.google.common.base.Objects;
+
+import de.brands4friends.daleq.internal.structure.TableStructure;
+
+public class TableContainer {
+    
+    private final TableStructure tableStructure;
+    private final List<RowContainer> rows;
+
+    public TableContainer(final TableStructure tableStructure, final List<RowContainer> rows) {
+        this.tableStructure = tableStructure;
+        this.rows = rows;
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if (obj instanceof TableContainer) {
+            final TableContainer that = (TableContainer) obj;
+
+            return Objects.equal(tableStructure, that.tableStructure)
+                    && Objects.equal(rows,that.rows);
+        }
+
+        return false;
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hashCode(tableStructure, rows);
+    }
 }
