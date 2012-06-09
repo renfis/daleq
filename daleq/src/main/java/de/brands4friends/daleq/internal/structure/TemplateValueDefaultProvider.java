1ebce1bc1a36e7f66b2c71644bbfde8e649afc89
Lars Girndt <l.girndt@googlemail.com>
2012-06-09 04:51:49 +0200

    sketched some idea how to handle template value generation

---
+++ daleq/src/main/java/de/brands4friends/daleq/internal/structure/TemplateValueDefaultProvider.java
@@ -0,0 +1,16 @@
+package de.brands4friends.daleq.internal.structure;
+
+public class TemplateValueDefaultProvider {
+
+//    private static Function<String,String> TO_NAMED_PREFIX = new Function<String, String>(){
+//        @Override
+//        public String apply(final String input) {
+//            return input + "-" + TemplateValue.DEFAULT;
+//        }
+//    };
+//
+//    public TemplateValue toDefault(final DataType dataType, final String fieldName) {
+//            return null;
+//        }
+//    }
+}
