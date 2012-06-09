7f291bca43fb8466467511414435e9e4115e7854
Lars Girndt <l.girndt@googlemail.com>
2012-06-09 14:03:16 +0200

    sketched implementation

--- daleq/src/main/java/de/brands4friends/daleq/internal/structure/TemplateValueDefaultProvider.java
+++ daleq/src/main/java/de/brands4friends/daleq/internal/structure/TemplateValueDefaultProvider.java
@@ -1,16 +1,105 @@
 package de.brands4friends.daleq.internal.structure;
 
+import static org.dbunit.dataset.datatype.DataType.BIGINT;
+import static org.dbunit.dataset.datatype.DataType.BIGINT_AUX_LONG;
+import static org.dbunit.dataset.datatype.DataType.BINARY;
+import static org.dbunit.dataset.datatype.DataType.BIT;
+import static org.dbunit.dataset.datatype.DataType.BLOB;
+import static org.dbunit.dataset.datatype.DataType.BOOLEAN;
+import static org.dbunit.dataset.datatype.DataType.CHAR;
+import static org.dbunit.dataset.datatype.DataType.CLOB;
+import static org.dbunit.dataset.datatype.DataType.DATE;
+import static org.dbunit.dataset.datatype.DataType.DECIMAL;
+import static org.dbunit.dataset.datatype.DataType.DOUBLE;
+import static org.dbunit.dataset.datatype.DataType.FLOAT;
+import static org.dbunit.dataset.datatype.DataType.INTEGER;
+import static org.dbunit.dataset.datatype.DataType.LONGNVARCHAR;
+import static org.dbunit.dataset.datatype.DataType.LONGVARBINARY;
+import static org.dbunit.dataset.datatype.DataType.LONGVARCHAR;
+import static org.dbunit.dataset.datatype.DataType.NCHAR;
+import static org.dbunit.dataset.datatype.DataType.NUMERIC;
+import static org.dbunit.dataset.datatype.DataType.NVARCHAR;
+import static org.dbunit.dataset.datatype.DataType.REAL;
+import static org.dbunit.dataset.datatype.DataType.SMALLINT;
+import static org.dbunit.dataset.datatype.DataType.TIME;
+import static org.dbunit.dataset.datatype.DataType.TIMESTAMP;
+import static org.dbunit.dataset.datatype.DataType.TINYINT;
+import static org.dbunit.dataset.datatype.DataType.VARBINARY;
+
+import java.util.Map;
+
+import org.dbunit.dataset.datatype.DataType;
+
+import com.google.common.collect.Maps;
+
+import de.brands4friends.daleq.DaleqBuildException;
+
 public class TemplateValueDefaultProvider {
 
-//    private static Function<String,String> TO_NAMED_PREFIX = new Function<String, String>(){
-//        @Override
-//        public String apply(final String input) {
-//            return input + "-" + TemplateValue.DEFAULT;
-//        }
-//    };
-//
-//    public TemplateValue toDefault(final DataType dataType, final String fieldName) {
-//            return null;
-//        }
-//    }
+    private static final DataType[] REGISTERED_DATA_TYPES = {
+            DataType.VARCHAR, CHAR, LONGVARCHAR, NCHAR, NVARCHAR, LONGNVARCHAR, CLOB, NUMERIC, DECIMAL, BOOLEAN, BIT,
+            INTEGER, TINYINT, SMALLINT, BIGINT, REAL, DOUBLE, FLOAT, DATE, TIME, TIMESTAMP,
+            VARBINARY, BINARY, LONGVARBINARY, BLOB,
+            //auxiliary types at the very end
+            BIGINT_AUX_LONG
+    };
+
+    public static interface ToTemplate {
+        TemplateValue map(String fieldName, String variable);
+    }
+
+    private static final ToTemplate STRING_TO_TEMPLATE = new ToTemplate() {
+        @Override
+        public TemplateValue map(final String fieldName, final String variable) {
+            return new TemplateValue(fieldName + "-" + variable);
+        }
+    };
+
+    private static ToTemplate NUMBER_TO_TEMPLATE = new ToTemplate() {
+        @Override
+        public TemplateValue map(final String fieldName, final String variable) {
+            return new TemplateValue(variable);
+        }
+    };
+
+    private final Map<DataType, ToTemplate> mapping;
+
+    private TemplateValueDefaultProvider(final Map<DataType, ToTemplate> mapping) {
+        this.mapping = mapping;
+    }
+
+    public TemplateValue toDefault(final DataType dataType, final String fieldName) {
+        final ToTemplate toTemplate = mapping.get(dataType);
+        if (toTemplate == null) {
+            String msg = String.format(
+                    "Cannot create a default TemplateValue for field %s, because the DataType %s is unknown.",
+                    fieldName, dataType
+            );
+            throw new DaleqBuildException(msg);
+        }
+        return toTemplate.map(fieldName, TemplateValue.VAR);
+    }
+
+    public static TemplateValueDefaultProvider create() {
+        final Map<DataType, ToTemplate> mapping = Maps.newHashMap();
+        for (DataType dataType : REGISTERED_DATA_TYPES) {
+            mapping.put(dataType, map(dataType));
+        }
+        return new TemplateValueDefaultProvider(mapping);
+    }
+
+    private static ToTemplate map(final DataType dataType) {
+        if (dataType.isNumber()) {
+            return NUMBER_TO_TEMPLATE;
+        }
+        if (dataType.equals(DataType.DATE)) {
+            // TODO
+            return null;
+        }
+        if (dataType.equals(DataType.TIME) || dataType.equals(DataType.TIMESTAMP)) {
+            // TODO
+            return null;
+        }
+        return STRING_TO_TEMPLATE;
+    }
 }
