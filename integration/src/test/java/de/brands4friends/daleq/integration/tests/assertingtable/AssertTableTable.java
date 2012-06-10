4ac888ec853d2404f3bf44b22881fc33dfb816a9
Lars Girndt <l.girndt@googlemail.com>
2012-06-10 15:15:15 +0200

    simple test

--- integration/src/test/java/de/brands4friends/daleq/integration/tests/assertingtable/AssertTableTable.java
+++ integration/src/test/java/de/brands4friends/daleq/integration/tests/assertingtable/AssertTableTable.java
@@ -1,7 +1,13 @@
 package de.brands4friends.daleq.integration.tests.assertingtable;
 
+import org.dbunit.dataset.datatype.DataType;
+
+import de.brands4friends.daleq.FieldDef;
 import de.brands4friends.daleq.TableDef;
 
 @TableDef("ASSERT_TABLE")
 public class AssertTableTable {
+    public static final FieldDef ID = FieldDef.fd(DataType.INTEGER);
+    public static final FieldDef NAME = FieldDef.fd(DataType.VARCHAR);
+    public static final FieldDef AMOUNT = FieldDef.fd(DataType.DECIMAL);
 }
--- integration/src/test/java/de/brands4friends/daleq/integration/tests/assertingtable/AssertTableTest.java
+++ integration/src/test/java/de/brands4friends/daleq/integration/tests/assertingtable/AssertTableTest.java
@@ -2,18 +2,29 @@ package de.brands4friends.daleq.integration.tests.assertingtable;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import de.brands4friends.daleq.Daleq;
+import de.brands4friends.daleq.Table;
 import de.brands4friends.daleq.integration.IntegrationConfig;
+import de.brands4friends.daleq.jdbc.DaleqSupport;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = IntegrationConfig.class)
 public class AssertTableTest extends AbstractTransactionalJUnit4SpringContextTests {
 
-    @Test
-    public void test() {
+    @Autowired
+    private DaleqSupport daleq;
 
+    @Test
+    public void inserting_build_asserting() {
+        Table toBeInserted = Daleq.aTable(AssertTableTable.class).withRowsUntil(100);
+        daleq.insertIntoDatabase(toBeInserted);
+
+        Table toBeCompared = Daleq.aTable(AssertTableTable.class).withRowsUntil(100);
+        daleq.assertTableInDatabase(toBeCompared);
     }
 }
