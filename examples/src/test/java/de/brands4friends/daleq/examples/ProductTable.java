9263a2be6dba6a8851127e21bb5ddabbc1635bcf
Lars Girndt <l.girndt@googlemail.com>
2012-06-16 15:26:34 +0200

    Added Apache License 2.0 as the project license

--- examples/src/test/java/de/brands4friends/daleq/examples/ProductTable.java
+++ examples/src/test/java/de/brands4friends/daleq/examples/ProductTable.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 brands4friends, Private Sale GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.brands4friends.daleq.examples;
 
 import org.dbunit.dataset.datatype.DataType;
@@ -7,8 +23,8 @@ import de.brands4friends.daleq.TableDef;
 
 @TableDef("PRODUCT")
 public class ProductTable {
-    public static final FieldDef ID    = FieldDef.fd(DataType.INTEGER);
-    public static final FieldDef NAME  = FieldDef.fd(DataType.VARCHAR);
-    public static final FieldDef SIZE  = FieldDef.fd(DataType.VARCHAR);
+    public static final FieldDef ID = FieldDef.fd(DataType.INTEGER);
+    public static final FieldDef NAME = FieldDef.fd(DataType.VARCHAR);
+    public static final FieldDef SIZE = FieldDef.fd(DataType.VARCHAR);
     public static final FieldDef PRICE = FieldDef.fd(DataType.DECIMAL);
 }
