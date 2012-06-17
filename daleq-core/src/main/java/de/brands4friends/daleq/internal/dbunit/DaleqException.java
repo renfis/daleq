e58d10044ec562d0583af18fa69ed4ed03daf961
Lars Girndt <l.girndt@googlemail.com>
2012-06-17 09:16:46 +0200

    removed unused ctors

--- daleq-core/src/main/java/de/brands4friends/daleq/internal/dbunit/DaleqException.java
+++ daleq-core/src/main/java/de/brands4friends/daleq/internal/dbunit/DaleqException.java
@@ -17,15 +17,6 @@
 package de.brands4friends.daleq.internal.dbunit;
 
 public class DaleqException extends RuntimeException {
-
-    public DaleqException(final String message) {
-        super(message);
-    }
-
-    public DaleqException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
-
     public DaleqException(final Throwable cause) {
         super(cause);
     }
