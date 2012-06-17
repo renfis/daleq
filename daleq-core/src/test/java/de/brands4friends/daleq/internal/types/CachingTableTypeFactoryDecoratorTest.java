bd2d704224ddacd2970b8bf6a6cacca56ebaff12
Lars Girndt <l.girndt@googlemail.com>
2012-06-17 09:02:28 +0200

    added caching decoratoro for table type creation

---
+++ daleq-core/src/test/java/de/brands4friends/daleq/internal/types/CachingTableTypeFactoryDecoratorTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2012 brands4friends, Private Sale GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.brands4friends.daleq.internal.types;
+
+import static org.easymock.EasyMock.expect;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.dbunit.dataset.datatype.DataType;
+import org.easymock.EasyMockSupport;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.brands4friends.daleq.FieldDef;
+import de.brands4friends.daleq.TableDef;
+
+public class CachingTableTypeFactoryDecoratorTest extends EasyMockSupport {
+
+    private TableTypeFactory delegate;
+    private CachingTableTypeFactoryDecorator decorator;
+    private TableType tableType;
+
+    @TableDef("THE_TABLE")
+    public static class TheTable {
+        public static final FieldDef ID = FieldDef.fd(DataType.INTEGER);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        delegate = createMock(TableTypeFactory.class);
+        decorator = new CachingTableTypeFactoryDecorator(delegate);
+        tableType = new TableTypeFactoryImpl().create(TheTable.class);
+    }
+
+    @Test
+    public void onCacheMiss_should_callDelegate() {
+        expectCallDelegate();
+
+        replayAll();
+        final TableType result = decorator.create(TheTable.class);
+        assertThat(result, is(tableType));
+        verifyAll();
+    }
+
+    @Test
+    public void onCacheHit_should_notDelegate() {
+        expectCallDelegate();
+
+        replayAll();
+        decorator.create(TheTable.class);
+        decorator.create(TheTable.class);
+        verifyAll();
+    }
+
+    private void expectCallDelegate() {
+        expect(delegate.create(TheTable.class)).andReturn(tableType).times(1);
+    }
+
+}
