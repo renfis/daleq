73248b5a588083ecccb4c165bda8b576d26d88bf
Lars Girndt <l.girndt@googlemail.com>
2012-06-17 12:55:21 +0200

    added UT

--- daleq-core/src/test/java/de/brands4friends/daleq/internal/builder/TableContainerImplTest.java
+++ daleq-core/src/test/java/de/brands4friends/daleq/internal/builder/TableContainerImplTest.java
@@ -17,15 +17,62 @@
 package de.brands4friends.daleq.internal.builder;
 
 import static nl.jqno.equalsverifier.EqualsVerifier.forClass;
+import static org.junit.Assert.assertThat;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import org.dbunit.dataset.datatype.DataType;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+
+import de.brands4friends.daleq.Daleq;
+import de.brands4friends.daleq.FieldDef;
+import de.brands4friends.daleq.NoSuchDaleqFieldException;
+import de.brands4friends.daleq.TableContainer;
+import de.brands4friends.daleq.TableDef;
 import nl.jqno.equalsverifier.Warning;
 
 public class TableContainerImplTest {
 
+    @TableDef("THE_TABLE")
+    public static class TheTable {
+        public static final FieldDef ID = FieldDef.fd(DataType.INTEGER);
+    }
+
     @Test
     public void testHashcodeAndEquals() {
         forClass(TableContainerImpl.class).suppress(Warning.NULL_FIELDS).verify();
+    }
+
+    @Test
+    public void getValuesOfField_ifFieldExists_should_returnThoseFields() {
+        final TableContainer table = Daleq.aTable(TheTable.class).withRowsUntil(5).build(new SimpleContext());
+        final List<Optional<String>> expected = Lists.transform(
+                Lists.newArrayList("0", "1", "2", "3", "4"),
+                new Function<String, Optional<String>>() {
+                    @Override
+                    public Optional<String> apply(@Nullable final String input) {
+                        if (input == null) {
+                            return Optional.absent();
+                        }
+                        return Optional.of(input);
+                    }
+                }
+        );
+        assertThat(Lists.newArrayList(table.getValuesOfField("ID")), Matchers.is(expected));
+    }
+
+    @Test(expected = NoSuchDaleqFieldException.class)
+    public void getValuesOfField_ifFieldDoesNotExist_sould_fail() {
+        final TableContainer table = Daleq.aTable(TheTable.class).withRowsUntil(5).build(new SimpleContext());
+        final Iterable<Optional<String>> result = table.getValuesOfField("DOES_NOT_EXIST");
+        // should have failed
+        assertThat(result, Matchers.nullValue());
     }
 }
