33ab4febe6b980779a5d601cdf2c5a11b01477e1
Lars Girndt <lars.girndt@brands4friends.de>
2012-09-01 22:25:04 +0200

    fixed travis_build issues

--- travis_build.sh
+++ travis_build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-./gradlew --stacktrace --quiet --continue --no-color \
--Pintegration.db=${INTEGRATION_DB}\
+./gradlew --stacktrace --continue --no-color \
+-Pintegration.db=${INTEGRATION_DB} \
 -Pintegration.mysql.url=jdbc:mysql://127.0.0.1/daleq_test \
 -Pintegration.mysql.user=root \
 -Pintegration.mysql.password= \
