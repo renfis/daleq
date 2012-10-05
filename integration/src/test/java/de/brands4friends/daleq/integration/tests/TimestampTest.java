364737608434e8e8bbceae2b3062226e68800306
Lars Girndt <lars.girndt@brands4friends.de>
2012-10-05 21:24:03 +0200

    Test on DbUnit Timestamp issue
    There's a DbUnit bug regarding milliseconds and zero truncation. We've
    added a unit test to show case this bug.
    
    http://sourceforge.net/tracker/index.php?func=detail&aid=3071408&group_id=47439&atid=449491

---
+++ integration/src/test/java/de/brands4friends/daleq/integration/tests/TimestampTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2012 brands4friends, Private Sale GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.brands4friends.daleq.integration.tests;
+
+import static de.brands4friends.daleq.core.Daleq.aRow;
+import static de.brands4friends.daleq.core.Daleq.aTable;
+
+import java.sql.Timestamp;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import de.brands4friends.daleq.core.Table;
+import de.brands4friends.daleq.integration.tables.TimestampTypeTable;
+
+public class TimestampTest extends BaseTest {
+
+    @Test
+    public void everyDataType_should_beInsertedIntoTheDatabase() {
+
+        final DateTime dateTime = new DateTime(2012, 10, 5, 16, 0, 0, 560);
+
+        final Timestamp ts = new Timestamp(dateTime.getMillis());
+        final Table table = aTable(TimestampTypeTable.class).with(
+                aRow(42).f(TimestampTypeTable.A_TIMESTAMP,ts)
+        );
+        daleq.insertIntoDatabase(table);
+
+        final Table expected = aTable(TimestampTypeTable.class).with(
+                aRow(42).f(TimestampTypeTable.A_TIMESTAMP,dateTime)
+        );
+
+        daleq.assertTableInDatabase(expected);
+
+    }
+
+
+}
