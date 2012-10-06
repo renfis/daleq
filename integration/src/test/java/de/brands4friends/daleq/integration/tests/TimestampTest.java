75e40f406bd599a2ca8acd4a76d850dbad2b10ca
Lars Girndt <lars.girndt@brands4friends.de>
2012-10-06 08:27:02 +0200

    Fixed failing Mysql Tests due to ms precision
    Mysql does not support milliseconds precision. Hence we cannot test
    whether it gets the same ms as we inserted.
    We've got no chance but to ignore these tests on Mysql.

--- integration/src/test/java/de/brands4friends/daleq/integration/tests/TimestampTest.java
+++ integration/src/test/java/de/brands4friends/daleq/integration/tests/TimestampTest.java
@@ -18,6 +18,7 @@ package de.brands4friends.daleq.integration.tests;
 
 import static de.brands4friends.daleq.core.Daleq.aRow;
 import static de.brands4friends.daleq.core.Daleq.aTable;
+import static de.brands4friends.daleq.integration.config.SupportedDb.MYSQL;
 
 import java.sql.Timestamp;
 
@@ -25,11 +26,13 @@ import org.joda.time.DateTime;
 import org.junit.Test;
 
 import de.brands4friends.daleq.core.Table;
+import de.brands4friends.daleq.integration.rules.Restrict;
 import de.brands4friends.daleq.integration.tables.TimestampTypeTable;
 
 public class TimestampTest extends BaseTest {
 
     @Test
+    @Restrict(not = MYSQL, reason = "Mysql does not support milliseconds precision in Timestamps")
     public void timestamp_should_haveProperMillis() {
 
         final DateTime dateTime = new DateTime(2012, 10, 5, 16, 0, 0, 560);
@@ -48,5 +51,23 @@ public class TimestampTest extends BaseTest {
 
     }
 
+    @Test
+    @Restrict(not = MYSQL, reason = "Mysql does not support milliseconds precision in Timestamps")
+    public void dateTime_should_haveProperMillis() {
+
+        final DateTime dateTime = new DateTime(2012, 10, 5, 16, 0, 0, 560);
+
+        final Table table = aTable(TimestampTypeTable.class).with(
+                aRow(42).f(TimestampTypeTable.A_TIMESTAMP, dateTime)
+        );
+        daleq.insertIntoDatabase(table);
+
+        final Table expected = aTable(TimestampTypeTable.class).with(
+                aRow(42).f(TimestampTypeTable.A_TIMESTAMP, dateTime)
+        );
+
+        daleq.assertTableInDatabase(expected);
+
+    }
 
 }
