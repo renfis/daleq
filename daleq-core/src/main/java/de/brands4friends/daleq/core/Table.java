b4b832e0cd7f7daccd836f2d9a1ac1534d26679f
Lars Girndt <lars.girndt@brands4friends.de>
2012-10-25 14:42:11 +0200

    initial documentation on Table

--- daleq-core/src/main/java/de/brands4friends/daleq/core/Table.java
+++ daleq-core/src/main/java/de/brands4friends/daleq/core/Table.java
@@ -18,10 +18,50 @@ package de.brands4friends.daleq.core;
 
 import javax.annotation.Nullable;
 
+/**
+ * Used to build the content of a relational database table.
+ * <p/>
+ * <code>Table</code> is a container which holds a list of {@link Row}s. Therefore it provides a set of builder methods to construct
+ * these rows.
+ * <p/>
+ * Use {@link Daleq#aTable} to create a <code>Table</code>. We encourage to static import this method to benefit from Daleq's
+ * embedded DSL.
+ */
 public interface Table {
 
+    /**
+     * Adds <code>rows</code> to the table.
+     * <p/>
+     * For instance
+     * <pre>{@code
+     * aTable(ProductTable.class)
+     *      .rows(
+     *          aRow(1).f(SIZE,"XL"),
+     *          aRow(2)
+     *      )
+     * }</pre>
+     * will yield a table containing
+     * <table>
+     * <thea
+     * <tr><th>ID</th><th>NAME</th><th>SIZE</th><th>PRICE</th></tr>
+     * <tr><td>1</td><td>?</td><td>XL</td><td>?</td></tr>
+     * <tr><td>2</td><td>?</td><td>?</td><td>?</td></tr>
+     * </table>
+     *
+     * @param rows all rows which should be added to the table
+     * @return a <code>Table</code> which has the new rows.
+     * @throws NullPointerException if one instance of <code>rows</code> is <code>null</code>.
+     */
     Table with(Row... rows);
 
+    /**
+     * Adds rows with the given <code>ids</code> to the table.
+     * <p/>
+     * A convenience method, which adds new rows to the table
+     *
+     * @param ids the ids of those rows, which will be added to the table.
+     * @return a <code>Table</code> which has the new rows.
+     */
     Table withSomeRows(Iterable<Long> ids);
 
     Table withSomeRows(long... ids);
