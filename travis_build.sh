c2722242ed6dae67dcc1f80b34ecba5bc2291a7d
Lars Girndt <lars.girndt@brands4friends.de>
2013-01-01 14:01:12 +0100

    Improved integration testing on travis
    Rather than testing each DB in as a separate environment,
    now our gradle build is able to accept a list of integration
    DBs and runs the test task for each DB.
    
    Use the Property integration.db and separate the DBs
    by ','.

--- travis_build.sh
+++ travis_build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ./gradlew --stacktrace --continue --quiet --no-color \
--Pintegration.db=${INTEGRATION_DB} \
+-Pintegration.db=HSQLDB,H2,Mysql \
 -Pintegration.mysql.url=jdbc:mysql://127.0.0.1/daleq_test \
 -Pintegration.mysql.user=root \
 -Pintegration.mysql.password= \
